3641f67baddccfc352147b70cb06e9ea0d9b6bc7
serfman <1sportick1@gmail.com>
2026-03-26 18:08:06 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,26 +33,26 @@ export default function RootLayout({
 
       {/* ✅ Яндекс Метрика */}
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108235719','ym');
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+          k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108235719','ym');
 
-              ym(108235719, 'init', {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-              });
-            `,
-          }}
-        />
-      </head>
+        ym(108235719, 'init', {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+        });
+      `,
+    }}
+  />
+</head>
 
       <body className="bg-dark text-parchment font-sans antialiased min-h-screen flex flex-col">
 
