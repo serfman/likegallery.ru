9656db1b57fcbda85fb8c057ef747d812aac9765
serfman <1sportick1@gmail.com>
2026-03-30 19:48:59 +0700

    Update index.ts

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -1,4 +1,5 @@
 import { item } from './item'
 import { serviceCase } from './serviceCase'
+import article from './article'
 
-export const schemaTypes = [item, serviceCase]
+export const schemaTypes = [item, serviceCase, article]
